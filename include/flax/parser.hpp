d3ec5a16b4725645bb4cfea3198c7d35cda8cbd9
myun2 <myun2@nwhite.info>
2013-04-05 18:32:57 +0900

    include/flax/parser.hpp Add

---
+++ include/flax/parser.hpp
@@ -0,0 +1,11 @@
+#ifndef __MYUN2_FLAX__flax__parser_hpp__
+#define __MYUN2_FLAX__flax__parser_hpp__
+
+namespace flax
+{
+	void parse(const char* script_str)
+	{
+	}
+}
+
+#endif//__MYUN2_FLAX__flax__parser_hpp__
